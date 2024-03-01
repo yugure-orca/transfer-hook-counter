d67145d91f3c06d3e55376d4856af313134bbdc3
yugure <109891005+yugure-orca@users.noreply.github.com>
2024-03-01 14:16:13 +0900

    initial commit

---
+++ programs/transfer-hook-counter/src/lib.rs
@@ -0,0 +1,177 @@
+// This code is initially copied from https://beta.solpg.io/github.com/solana-developers/anchor-transfer-hook/tree/counter
+
+use anchor_lang::{
+    prelude::*,
+    system_program::{create_account, CreateAccount},
+};
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{Mint, TokenAccount, TokenInterface},
+};
+use spl_tlv_account_resolution::{
+    account::ExtraAccountMeta, seeds::Seed, state::ExtraAccountMetaList,
+};
+use spl_transfer_hook_interface::instruction::{ExecuteInstruction, TransferHookInstruction};
+
+declare_id!("EBZDYx7599krFc4m2govwBdZcicr4GgepqC78m71nsHS");
+
+#[error_code]
+pub enum MyError {
+    #[msg("The amount is too big")]
+    AmountTooBig,
+}
+
+#[program]
+pub mod transfer_hook_counter {
+    use super::*;
+
+    pub fn initialize_extra_account_meta_list(
+        ctx: Context<InitializeExtraAccountMetaList>,
+    ) -> Result<()> {
+
+        // The `addExtraAccountsToInstruction` JS helper function resolving incorrectly
+        let account_metas = vec![
+            ExtraAccountMeta::new_with_seeds(
+                &[
+                    Seed::Literal {bytes: "counter".as_bytes().to_vec()},
+                    Seed::AccountKey { index: 1 }, // mint
+                ],
+                false, // is_signer
+                true,  // is_writable
+            )?,
+        ];
+
+        // calculate account size
+        let account_size = ExtraAccountMetaList::size_of(account_metas.len())? as u64;
+        // calculate minimum required lamports
+        let lamports = Rent::get()?.minimum_balance(account_size as usize);
+
+        let mint = ctx.accounts.mint.key();
+        let signer_seeds: &[&[&[u8]]] = &[&[
+            b"extra-account-metas",
+            &mint.as_ref(),
+            &[ctx.bumps.extra_account_meta_list],
+        ]];
+
+        // create ExtraAccountMetaList account
+        create_account(
+            CpiContext::new(
+                ctx.accounts.system_program.to_account_info(),
+                CreateAccount {
+                    from: ctx.accounts.payer.to_account_info(),
+                    to: ctx.accounts.extra_account_meta_list.to_account_info(),
+                },
+            )
+            .with_signer(signer_seeds),
+            lamports,
+            account_size,
+            ctx.program_id,
+        )?;
+
+        // initialize ExtraAccountMetaList account with extra accounts
+        ExtraAccountMetaList::init::<ExecuteInstruction>(
+            &mut ctx.accounts.extra_account_meta_list.try_borrow_mut_data()?,
+            &account_metas,
+        )?;
+
+        Ok(())
+    }
+
+    pub fn transfer_hook(ctx: Context<TransferHook>, amount: u64) -> Result<()> {
+
+        if amount > 50 /* note: u64 amount */ {
+            msg!("The amount is too big {0}", amount);
+        //    return err!(MyError::AmountTooBig);
+        }
+
+        ctx.accounts.counter_account.counter += 1;
+
+        msg!("This token has been transfered {0} times", ctx.accounts.counter_account.counter);
+       
+        Ok(())
+    }
+
+    // fallback instruction handler as workaround to anchor instruction discriminator check
+    pub fn fallback<'info>(
+        program_id: &Pubkey,
+        accounts: &'info [AccountInfo<'info>],
+        data: &[u8],
+    ) -> Result<()> {
+        let instruction = TransferHookInstruction::unpack(data)?;
+
+        // match instruction discriminator to transfer hook interface execute instruction  
+        // token2022 program CPIs this instruction on token transfer
+        match instruction {
+            TransferHookInstruction::Execute { amount } => {
+                let amount_bytes = amount.to_le_bytes();
+
+                // invoke custom transfer hook instruction on our program
+                __private::__global::transfer_hook(program_id, accounts, &amount_bytes)
+            }
+            _ => return Err(ProgramError::InvalidInstructionData.into()),
+        }
+    }
+}
+
+#[derive(Accounts)]
+pub struct InitializeExtraAccountMetaList<'info> {
+    #[account(mut)]
+    payer: Signer<'info>,
+
+    /// CHECK: ExtraAccountMetaList Account, must use these seeds
+    #[account(
+        mut,
+        seeds = [b"extra-account-metas", mint.key().as_ref()], 
+        bump
+    )]
+    pub extra_account_meta_list: AccountInfo<'info>,
+    pub mint: InterfaceAccount<'info, Mint>,
+    #[account(
+        init_if_needed,
+        seeds = [b"counter", mint.key().as_ref()], 
+        bump,
+        payer = payer,
+        space = 8 + 4
+    )]
+    pub counter_account: Account<'info, CounterAccount>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
+// Order of accounts matters for this struct.
+// The first 4 accounts are the accounts required for token transfer (source, mint, destination, owner)
+// Remaining accounts are the extra accounts required from the ExtraAccountMetaList account
+// These accounts are provided via CPI to this program from the token2022 program
+#[derive(Accounts)]
+pub struct TransferHook<'info> {
+    #[account(
+        token::mint = mint, 
+        token::authority = owner,
+    )]
+    pub source_token: InterfaceAccount<'info, TokenAccount>,
+    pub mint: InterfaceAccount<'info, Mint>,
+    #[account(
+        token::mint = mint,
+    )]
+    pub destination_token: InterfaceAccount<'info, TokenAccount>,
+    /// CHECK: source token account owner, can be SystemAccount or PDA owned by another program
+    pub owner: UncheckedAccount<'info>,
+    /// CHECK: ExtraAccountMetaList Account,
+    #[account(
+        seeds = [b"extra-account-metas", mint.key().as_ref()], 
+        bump
+    )]
+    pub extra_account_meta_list: UncheckedAccount<'info>,
+    #[account(
+        mut,
+        seeds = [b"counter", mint.key().as_ref()],
+        bump
+    )]
+    pub counter_account: Account<'info, CounterAccount>,
+}
+
+#[account]
+pub struct CounterAccount {
+    counter: u32,
+}
---
+++ tests/transfer-hook-counter.ts
@@ -0,0 +1,256 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { TransferHookCounter } from "../target/types/transfer_hook_counter";
+import {
+  PublicKey,
+  SystemProgram,
+  Transaction,
+  sendAndConfirmTransaction,
+  Keypair,
+} from "@solana/web3.js";
+import {
+  ExtensionType,
+  TOKEN_2022_PROGRAM_ID,
+  getMintLen,
+  createInitializeMintInstruction,
+  createInitializeTransferHookInstruction,
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  createAssociatedTokenAccountInstruction,
+  createMintToInstruction,
+  getAssociatedTokenAddressSync,
+  createTransferCheckedWithTransferHookInstruction,
+  getExtraAccountMetas
+} from "@solana/spl-token";
+
+describe("transfer-hook-counter", () => {
+  // Configure the client to use the local cluster.
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.TransferHookCounter as Program<TransferHookCounter>;
+  const wallet = provider.wallet as anchor.Wallet;
+  const connection = provider.connection;
+
+  // Generate keypair to use as address for the transfer-hook enabled mint
+  const mint = new Keypair();
+  const decimals = 9;
+
+  // Sender token account address
+  const sourceTokenAccount = getAssociatedTokenAddressSync(
+    mint.publicKey,
+    wallet.publicKey,
+    false,
+    TOKEN_2022_PROGRAM_ID,
+    ASSOCIATED_TOKEN_PROGRAM_ID
+  );
+
+  // Recipient token account address
+  const recipient = Keypair.generate();
+  const destinationTokenAccount = getAssociatedTokenAddressSync(
+    mint.publicKey,
+    recipient.publicKey,
+    false,
+    TOKEN_2022_PROGRAM_ID,
+    ASSOCIATED_TOKEN_PROGRAM_ID
+  );
+
+  // ExtraAccountMetaList address
+  // Store extra accounts required by the custom transfer hook instruction
+  const [extraAccountMetaListPDA] = PublicKey.findProgramAddressSync(
+    [Buffer.from("extra-account-metas"), mint.publicKey.toBuffer()],
+    program.programId
+  );
+  
+  const [counterPDA] = PublicKey.findProgramAddressSync(
+    [Buffer.from("counter"), mint.publicKey.toBuffer()],
+    program.programId
+  );
+
+  it("Create Mint Account with Transfer Hook Extension", async () => {
+    const extensions = [ExtensionType.TransferHook];
+    const mintLen = getMintLen(extensions);
+    const lamports =
+      await provider.connection.getMinimumBalanceForRentExemption(mintLen);
+
+    const transaction = new Transaction().add(
+      SystemProgram.createAccount({
+        fromPubkey: wallet.publicKey,
+        newAccountPubkey: mint.publicKey,
+        space: mintLen,
+        lamports: lamports,
+        programId: TOKEN_2022_PROGRAM_ID,
+      }),
+      createInitializeTransferHookInstruction(
+        mint.publicKey,
+        wallet.publicKey,
+        program.programId, // Transfer Hook Program ID
+        TOKEN_2022_PROGRAM_ID
+      ),
+      createInitializeMintInstruction(
+        mint.publicKey,
+        decimals,
+        wallet.publicKey,
+        null,
+        TOKEN_2022_PROGRAM_ID
+      )
+    );
+
+    const txSig = await sendAndConfirmTransaction(
+      provider.connection,
+      transaction,
+      [wallet.payer, mint],
+      { skipPreflight: true, commitment: "confirmed"}
+    );
+
+    console.log(`Transaction Signature: ${txSig}`);
+  });
+
+  // Create the two token accounts for the transfer-hook enabled mint
+  // Fund the sender token account with 100 tokens
+  it("Create Token Accounts and Mint Tokens", async () => {
+    // 100 tokens
+    const amount = 100 * 10 ** decimals;
+
+    const transaction = new Transaction().add(
+      createAssociatedTokenAccountInstruction(
+        wallet.publicKey,
+        sourceTokenAccount,
+        wallet.publicKey,
+        mint.publicKey,
+        TOKEN_2022_PROGRAM_ID,
+        ASSOCIATED_TOKEN_PROGRAM_ID
+      ),
+      createAssociatedTokenAccountInstruction(
+        wallet.publicKey,
+        destinationTokenAccount,
+        recipient.publicKey,
+        mint.publicKey,
+        TOKEN_2022_PROGRAM_ID,
+        ASSOCIATED_TOKEN_PROGRAM_ID
+      ),
+      createMintToInstruction(
+        mint.publicKey,
+        sourceTokenAccount,
+        wallet.publicKey,
+        amount,
+        [],
+        TOKEN_2022_PROGRAM_ID
+      )
+    );
+
+    const txSig = await sendAndConfirmTransaction(
+      connection,
+      transaction,
+      [wallet.payer],
+      { skipPreflight: true, commitment: "confirmed"}
+    );
+
+    console.log(`Transaction Signature: ${txSig}`);
+  });
+
+  // Account to store extra accounts required by the transfer hook instruction
+  it("Create ExtraAccountMetaList Account", async () => {
+    const extraAccountMetasInfo = await connection.getAccountInfo(extraAccountMetaListPDA);
+    
+    console.log("Extra accounts meta: " + extraAccountMetasInfo);
+
+    if (extraAccountMetasInfo === null) {
+      const initializeExtraAccountMetaListInstruction = await program.methods
+      .initializeExtraAccountMetaList()
+      .accounts({
+        mint: mint.publicKey,
+        extraAccountMetaList: extraAccountMetaListPDA,
+        counterAccount: counterPDA,
+      })
+      .instruction();
+
+      const transaction = new Transaction().add(
+        initializeExtraAccountMetaListInstruction
+      );
+
+      const txSig = await sendAndConfirmTransaction(
+        provider.connection,
+        transaction,
+        [wallet.payer],
+        { skipPreflight: true, commitment: "confirmed"}
+      );
+      console.log("Transaction Signature:", txSig);
+    }
+
+  });
+
+  it("Transfer Hook with Extra Account Meta", async () => {
+    // 2 tokens
+    const amount = 2 * 10 ** decimals;
+    const amountBigInt = BigInt(amount);
+
+    const preCounterState = await program.account.counterAccount.fetch(counterPDA, "confirmed");
+    const preSourceBalance = await connection.getTokenAccountBalance(sourceTokenAccount, "confirmed");
+    const preDestinationBalance = await connection.getTokenAccountBalance(destinationTokenAccount, "confirmed");
+    
+    let transferInstructionWithHelper = await createTransferCheckedWithTransferHookInstruction( 
+      connection,
+      sourceTokenAccount,
+      mint.publicKey,
+      destinationTokenAccount,
+      wallet.publicKey,
+      amountBigInt,
+      decimals,
+      [],
+      "confirmed",
+      TOKEN_2022_PROGRAM_ID,
+    );
+
+    console.log("Extra accounts meta: " + extraAccountMetaListPDA);
+    console.log("Counter PDa: " + counterPDA);
+    console.log("Transfer Instruction: " + JSON.stringify(transferInstructionWithHelper));
+    
+    const transaction1 = new Transaction().add(
+      transferInstructionWithHelper
+    );
+
+    const txSig1 = await sendAndConfirmTransaction(
+      connection,
+      transaction1,
+      [wallet.payer],
+      { skipPreflight: true, commitment: "confirmed" }
+    );
+    console.log("Transfer Signature 1:", txSig1);
+
+    const post1CounterState = await program.account.counterAccount.fetch(counterPDA, "confirmed");
+    const post1SourceBalance = await connection.getTokenAccountBalance(sourceTokenAccount, "confirmed");
+    const post1DestinationBalance = await connection.getTokenAccountBalance(destinationTokenAccount, "confirmed");
+
+    const sleep = (milliseconds: number) => { return new Promise(resolve => setTimeout(resolve, milliseconds)); };
+    await sleep(5000);
+
+    const transaction2 = new Transaction().add(
+      transferInstructionWithHelper
+    );
+
+    const txSig2 = await sendAndConfirmTransaction(
+      connection,
+      transaction2,
+      [wallet.payer],
+      { skipPreflight: true, commitment: "confirmed" }
+    );
+    console.log("Transfer Signature 2:", txSig2);
+
+    const post2CounterState = await program.account.counterAccount.fetch(counterPDA, "confirmed");
+    const post2SourceBalance = await connection.getTokenAccountBalance(sourceTokenAccount, "confirmed");
+    const post2DestinationBalance = await connection.getTokenAccountBalance(destinationTokenAccount, "confirmed");
+
+    console.log("Counter value: "
+      + preCounterState.counter + " -> "
+      + post1CounterState.counter + " -> "
+      + post2CounterState.counter);
+    console.log("Soruce balance: "
+      + preSourceBalance.value.uiAmountString + " -> " 
+      + post1SourceBalance.value.uiAmountString + " -> "
+      + post2SourceBalance.value.uiAmountString);
+    console.log("Destination balance: "
+      + preDestinationBalance.value.uiAmountString + " -> "
+      + post1DestinationBalance.value.uiAmountString + " -> "
+      + post2DestinationBalance.value.uiAmountString);    
+  });
+});
